3121c1b6341b9adfc9d6667ec4453e3f67f810f8
vinic <vinic@DESKTOP-8JUVNTD>
2022-02-16 15:29:02 -0300

    commit inicial do projeto

---
+++ aulasjsfprimefaces/src/br/com/framework/implementacao/crud/VariavelConecaoUtil.java
@@ -0,0 +1,12 @@
+package br.com.framework.implementacao.crud;
+
+/**
+ * Nome do caminho do JNDI datasource Tomcat
+ * @author vinic
+ *
+ */
+
+public class VariavelConecaoUtil {
+	
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbs/datasource";
+}
